d67ffb45efb0dcec2c783e46abe573ba54b53f59
Updesh Yadav <updeshdeepu1506@gmail.com>
2025-05-03 12:08:51 +0530

    a little change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from"react-router-dom"
-import Landing from './pages/landing'
+import Landing from './pages/Landing'
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
